c3f873ce1b31f6030c1fd394255748b1582b7b49
Sampson Orson Jackson <sampson.orson@gmail.com>
2021-10-10 10:10:11 +0100

    completed basic tryout of ngrx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,76 @@
 import { Component } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Post } from './models/post.model';
+import * as PostActions from './actions/post.actions';
+
+
+
+interface AppState {
+  message: string;
+  post: Post;
+}
+
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+    {{ message$ | async }}
+
+    <br />
+    <button (click)="spanishMessage()">Spanish</button>
+    <button (click)="frenchMessage()">French</button>
+    <hr />
+    <div *ngIf="post$ | async as p">
+      <h2>{{ p.text }}</h2>
+      <h4>Votes: {{ p.likes }}</h4>
+
+      <button (click)="upvote()">Upvote</button>
+      <button (click)="downvote()">Downvote</button>
+      <button (click)="resetPost()">Reset</button>
+
+      <br />
+      <input [(ngModel)]="text" />
+      <button (click)="editText()">Change Title</button>
     </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
   `,
-  styles: []
+  styles: [`
+    i { font-size: 2em; }
+    button { margin-bottom: 10px; }
+  `]
 })
 export class AppComponent {
-  title = 'ng-ngrx-hello';
+  message$: Observable<string>;
+  post$: Observable<Post>;
+
+  text!: string;
+
+  constructor(private store: Store<AppState>) {
+    this.message$ = this.store.select('message');
+    this.post$ = this.store.select('post');
+  }
+
+  spanishMessage() {
+    this.store.dispatch({ type: 'SPANISH' });
+  }
+
+  frenchMessage() {
+    this.store.dispatch({ type: 'FRENCH' });
+  }
+
+  editText() {
+    this.store.dispatch(new PostActions.EditText(this.text));
+  }
+
+  upvote() {
+    this.store.dispatch(new PostActions.Upvote());
+  }
+
+  downvote() {
+    this.store.dispatch(new PostActions.Downvote());
+  }
+
+  resetPost() {
+    this.store.dispatch(new PostActions.Reset());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { StoreModule } from '@ngrx/store';
+import { simpleReducer } from './reducers/simple.reducer';
+import { postReducer } from './reducers/post.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot({
+      message: simpleReducer,
+      post: postReducer
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 10 // number of state trees to save at any given time
+    })
+  ],
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
